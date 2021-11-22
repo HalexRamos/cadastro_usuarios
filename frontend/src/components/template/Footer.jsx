a67a8a7bb6505c4f185cd2abae2a167d84ddf84b
HalexRamos <halexramos96@gmail.com>
2021-11-22 00:44:22 -0300

    corrigindo espaço

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -6,7 +6,7 @@ export default (props) => (
     <span>
       Desenvolvido com <i className="fa fa-heart text-danger"></i> por
       <strong>
-        Cod<span className="text-danger">3</span>r
+        &#160; Cod<span className="text-danger">3</span>r
       </strong>
     </span>
   </footer>
